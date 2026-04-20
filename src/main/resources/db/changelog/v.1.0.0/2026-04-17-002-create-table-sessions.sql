2bc9da68f0383bc9b4e28dad3c9f1a1a8e788ba9
Alext_v <sharman2fire.90@yandex.ru>
2026-04-20 19:14:48 +0300

    fix: add table existence check

--- src/main/resources/db/changelog/v.1.0.0/2026-04-17-002-create-table-sessions.sql
+++ src/main/resources/db/changelog/v.1.0.0/2026-04-17-002-create-table-sessions.sql
@@ -1,6 +1,8 @@
 --liquibase formatted sql
 
 --changeset Alext_v:create-table-sessions
+--preconditions onFail:MARK_RAN
+--precondition-sql-check expectedResult:0 SELECT count(*) FROM information_schema.tables WHERE table_name = 'sessions'
 CREATE TABLE sessions (
     id UUID NOT NULL,
     user_id INT NOT NULL,
