64b0739f98a97d129e7325544e143d3cea4c57eb
Alext_v <sharman2fire.90@yandex.ru>
2026-04-29 21:08:28 +0300

    feat: add project constants

---
+++ src/main/java/com/dev/util/ProjectConstants.java
@@ -0,0 +1,12 @@
+package com.dev.util;
+
+public class ProjectConstants {
+    public static final String SESSION_ID = "session_id";
+    public static final int COOKIE_EXPIRATION_TIME_IN_SECONDS = 2 * 60;
+    public static final int SESSION_EXPIRATION_TIME_IN_MINUTES = 10;
+    public static final int DELETE_COOKIE_MAX_AGE = 0;
+
+    //Paths
+    public static final String PATH_SIGN_IN = "/sign-in";
+    public static final String PATH_SIGN_UP = "/sign-up";
+}
