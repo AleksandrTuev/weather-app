10dd587964feeb7009a578f5a61b856f257d56d5
Alext_v <sharman2fire.90@yandex.ru>
2026-05-01 11:34:31 +0300

    fix: logout cookie

--- src/main/java/com/dev/controller/SignOutController.java
+++ src/main/java/com/dev/controller/SignOutController.java
@@ -2,14 +2,11 @@ package com.dev.controller;
 
 import com.dev.service.UserService;
 import jakarta.servlet.http.Cookie;
-import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.Arrays;
 
 import static com.dev.util.ProjectConstants.SESSION_ID;
 
@@ -20,16 +17,11 @@ public class SignOutController {
     private final UserService userService;
 
     @PostMapping
-    public String signOut(HttpServletRequest req, HttpServletResponse resp) {
-        Cookie newCookie = Arrays.stream(req.getCookies())
-                .filter(cookie -> SESSION_ID.equals(cookie.getName()))
-                .findFirst()
-                .orElse(null);
-
-        if ((newCookie != null) && (newCookie.getValue() != null)) {
-            newCookie = userService.signOut(newCookie.getValue());
-            resp.addCookie(newCookie);
-        }
+    public String signOut(HttpServletResponse resp) {
+        Cookie cookie = new Cookie(SESSION_ID, null);
+        cookie.setPath("/");
+        cookie.setMaxAge(0);
+        resp.addCookie(cookie);
         return "redirect:/";
     }
 }
