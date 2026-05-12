31227be9fab6efd615dc24f61825d72dd3d742b1
Alext_v <sharman2fire.90@yandex.ru>
2026-05-12 12:15:54 +0300

    refactor: integration tests

--- src/test/java/UserServiceIntegrationTest.java
+++ src/test/java/UserServiceIntegrationTest.java
@@ -2,12 +2,16 @@ import com.dev.config.LiquibaseConfig;
 import com.dev.config.SpringConfig;
 import com.dev.dto.UserSignInDto;
 import com.dev.dto.UserSignUpDto;
+import com.dev.exception.InvalidPasswordException;
+import com.dev.exception.UserNotFoundException;
+import com.dev.exception.UsernameAlreadyExistsException;
 import com.dev.mapper.UserMapper;
 import com.dev.model.User;
 import com.dev.repository.UserRepository;
 import com.dev.service.UserService;
 import com.dev.util.ProjectConstants;
 import jakarta.servlet.http.Cookie;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,6 +30,8 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 @WebAppConfiguration
 @Transactional
 public class UserServiceIntegrationTest {
+    private static final String USERNAME = "ivan";
+    private static final String PASSWORD = "0123456789";
 
     @Autowired
     private UserService userService;
@@ -40,35 +46,72 @@ public class UserServiceIntegrationTest {
 
     @Test
     void testSignUp(){
-        User user = new User(null, "bobby", "0123456789");
+        Cookie cookie = userService.signUp(createUserSignUpDto());
 
-        UserSignUpDto userSignUpDto = userMapper.toUserSignUpDto(user);
-
-        Cookie cookie = userService.signUp(userSignUpDto);
         assertNotNull(cookie);
         assertEquals(ProjectConstants.SESSION_ID, cookie.getName());
         assertNotNull(cookie.getValue());
-
-        Integer count = jdbcTemplate.queryForObject("SELECT COUNT(*) FROM users", Integer.class);
-        assertEquals(1, count);
-
-        count = jdbcTemplate.queryForObject("SELECT COUNT(*) FROM sessions", Integer.class);
-        assertEquals(1, count);
+        assertEquals(1, getCountUsers());
+        assertEquals(1, getCountSessions());
     }
 
     @Test
     void testLogIn(){
-        User user = new User(null, "tom","0123456789");
-
+        User user = createUser();
         UserSignInDto userSignInDto = userMapper.toUserSignInDto(user);
-        user.setPassword(passwordEncoder.encode("0123456789"));
+        user.setPassword(passwordEncoder.encode(PASSWORD));
         userRepository.save(user);
-
         Cookie cookie = userService.signIn(userSignInDto);
+
         assertEquals(user.getUsername(), userRepository.findByName(user.getUsername()).get().getUsername());
         assertNotNull(cookie);
+        assertEquals(1, getCountSessions());
+    }
 
-        Integer count = jdbcTemplate.queryForObject("SELECT COUNT(*) FROM sessions", Integer.class);
-        assertEquals(1, count);
+    @Test
+    void shouldThrowUsernameAlreadyExistsException_WhenNonUniqueUserRegistration() {
+        userService.signUp(createUserSignUpDto());
+        Assertions.assertThrows(UsernameAlreadyExistsException.class, () -> {
+            userService.signUp(createUserSignUpDto());
+        });
+    }
+
+    @Test
+    void shouldThrowInvalidPasswordException_WhenPasswordIsNotValid() {
+        Cookie cookie = userService.signUp(createUserSignUpDto());
+        userService.signOut(cookie.getValue());
+        UserSignInDto user = createUserSignInDto();
+        user.setPassword("123");
+
+        Assertions.assertThrows(InvalidPasswordException.class, () -> {
+            userService.signIn(user);
+        });
+    }
+
+    @Test
+    void shouldThrowUserNotFoundException_WhenUserIsNotRegistered() {
+        Assertions.assertThrows(UserNotFoundException.class, () -> {
+            userService.signIn(createUserSignInDto());
+        });
+    }
+
+    private User createUser() {
+        return new User(null, USERNAME, PASSWORD);
+    }
+
+    private UserSignInDto createUserSignInDto() {
+        return new UserSignInDto(USERNAME, PASSWORD);
+    }
+
+    private UserSignUpDto createUserSignUpDto() {
+        return new UserSignUpDto(USERNAME, PASSWORD, PASSWORD);
+    }
+
+    private Integer getCountUsers() {
+        return jdbcTemplate.queryForObject("SELECT COUNT(*) FROM users", Integer.class);
+    }
+
+    private Integer getCountSessions() {
+       return  jdbcTemplate.queryForObject("SELECT COUNT(*) FROM sessions", Integer.class);
     }
 }
